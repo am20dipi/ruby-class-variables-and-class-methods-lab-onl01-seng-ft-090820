3b7aa54dfe3f12f7304302049e76565a89d4304f
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-18 21:16:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name, @artist, @genre = name, artist, genre
+    @@count += 1
+    @@genres << genre #contains all of the genres of existing songs
+    @@artists << artist #contains all of the artists of existing songs
+  end
+  
+  def self.count #is a class method that returns a number of songs created
+    @@count
+  end
+  
+  def self.artists #is a class method that returns a unique array of artists
+    @@artists.uniq
+  end
+  
+  def self.genres #is a class method that returns a unique array of genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    @@genres.each do |keys, values|
+      @@genres_count[keys] = values.count
+      @@genres << genre
+    end
+  end
+  
+  def self.artist_count
+  end
+end
+
+
+
